544a4ba8bdd3e4a392e6b6ec6a0112db1e26dee9
kira <chitimellapraneeth@gmail.com>
2023-11-17 00:15:03 +0530

    day-1

--- datatype.ts
+++ datatype.ts
@@ -7,3 +7,20 @@ let age : number;
 age = 25.56; //can give decimals for number datatype
 let res = parseInt("25"); //typecasting
 
+// let valid : boolean
+// console.log(valid) // error in ts but not in js as js dont know types as we used strict:true in ts config.json file
+
+//Two ways to use arrays in ts
+let empList : string[]; //using square brackets with datatype
+
+let numList : Array<number>; //using generics
+
+empList = ["John", "Mike"];
+
+numList = [90, 7, 1, 4];
+
+let ans = numList.filter((num)=>num>2);
+console.log(ans); //[90, 7, 4]
+
+let sum = numList.reduce((acc, num)=>acc+num) //reduce the arr to its sum 102
+console.log(sum)
