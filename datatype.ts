e6698daa525968b44687e26205ab31de2941516a
kira <chitimellapraneeth@gmail.com>
2023-11-17 00:01:34 +0530

    day 1

--- datatype.ts
+++ datatype.ts
@@ -2,3 +2,8 @@ let lname : string;
 lname = "prans";
 let newname = lname.toUpperCase();
 console.log(newname);
+
+let age : number;
+age = 25.56; //can give decimals for number datatype
+let res = parseInt("25"); //typecasting
+
